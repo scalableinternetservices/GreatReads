2d0f58a74bb1fb6bbad2bf6f72ad9c38ef8ab8b3
Akshay Bakshi <bakshiakshay94@gmail.com>
2015-10-29 15:21:40 -0700

    Added Shelf model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :shelves
   resources :on_shelves
   devise_for :users
   resources :users
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029220505) do
+ActiveRecord::Schema.define(version: 20151029221817) do
 
   create_table "books", force: :cascade do |t|
     t.datetime "created_at",          null: false
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 20151029220505) do
     t.integer  "shelf_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "shelves", force: :cascade do |t|
+    t.integer  "shelf_id"
+    t.string   "shelf_name"
+    t.integer  "shelf_owner"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
