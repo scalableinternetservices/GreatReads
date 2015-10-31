6a40b79326fcc8fcab046f10e8817cfb5aaac1c5
Aman Agarwal <aman.agarwal.2718@gmail.com>
2015-10-31 14:19:04 -0700

    working on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.delete_all
+
+aman = User.new
+aman.email = "aman@aman.com"
+aman.password = "amanaman"
+aman.password_confirmation = "amanaman"
+aman.save!
+
+lowell = User.new
+lowell.email = "lowell@lowell.com"
+lowell.password = "lowelllowell"
+lowell.password_confirmation = "lowelllowell"
+lowell.save!
+
+akshay = User.new
+akshay.email = "akshay@akshay.com"
+akshay.password = "akshayakshay"
+akshay.password_confirmation = "akshayakshay"
+akshay.save!
+
+chang = User.new
+chang.email = "chang@chang.com"
+chang.password = "changchang"
+chang.password_confirmation = "changchang"
+chang.save!
+
