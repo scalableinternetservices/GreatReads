95e2a004291ba10b505da5817825a7fe986a08bd
Aman Agarwal <aman.agarwal.2718@gmail.com>
2015-11-24 14:40:12 -0800

    Revert "EC2 initialize script and changed paperclip settings to accept PDF files"
    This reverts commit d1d5a7b8444a23d0f37cc82f0bfc10970d2212ae.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,4 @@
 class Book < ActiveRecord::Base
-	has_attached_file :pdf, styles: {thumbnail: "60x60#"}
-  # has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  # validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
-  # validates_attachment_content_type :document, content_type: "application/pdf"
-  validates_attachment_content_type :pdf, :content_type => ['application/pdf'], :if => :pdf_attached?
-
-  def pdf_attached?
-    self.pdf.file?
-  end
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
