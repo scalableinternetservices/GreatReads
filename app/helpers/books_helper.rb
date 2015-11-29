51fae1705d6d9eb65fd867f2308c94d19a89355c
Chang Liu <shampliu@gmail.com>
2015-11-29 15:40:43 -0800

    Changed cache key method for Book table / rows

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,9 @@
 module BooksHelper
+	def cache_key_for_book_row(book)
+		"book-#{book.id}-#{book.updated_at}-#{book.comments.count}"
+	end
+
+	def cache_key_for_book_table
+		"book-table-#{Book.maximum(:updated_at)}-#{Comment.maximum(:updated_at)}"
+	end
 end
